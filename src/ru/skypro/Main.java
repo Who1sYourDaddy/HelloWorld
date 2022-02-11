2dd4ec614a6295a1b8d63585223cb46381bd1179
Who1sYourDaddy <forymoney@yandex.ru>
2022-02-11 19:00:30 +0500

    init

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,7 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("   Hello World. This is new test for my brain. Let's Rock!");
+	System.out.println("   Hello World. This is new test for my brain. Let's Rock!" +
+    "When I tryied to commit file, I saw mistake like Select files to commit and i decided that i should do some changes. So you can read them!");
     }
 }
