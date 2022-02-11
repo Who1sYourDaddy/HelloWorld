14fe467e0b00a4f7a22909cd84dd841601f8048a
Who1sYourDaddy <forymoney@yandex.ru>
2022-02-11 19:11:08 +0500

    Alex Aramian New Branch commit

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,7 +3,6 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("   Hello World. This is new test for my brain. Let's Rock!" +
-    "When I tryied to commit and puch file, I got the mistake Select files to commit and i decided that i should do some changes. So you can read them!");
+	System.out.println("This is the best code for new branch");
     }
 }
