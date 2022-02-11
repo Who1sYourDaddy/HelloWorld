22e5fb8423e71b56caa571de4ba4fdedbcab1377
Who1sYourDaddy <forymoney@yandex.ru>
2022-02-11 19:02:07 +0500

    My first commit

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("   Hello World. This is new test for my brain. Let's Rock!" +
-    "When I tryied to commit file, I saw mistake like Select files to commit and i decided that i should do some changes. So you can read them!");
+    "When I tryied to commit and puch file, I got the mistake Select files to commit and i decided that i should do some changes. So you can read them!");
     }
 }
